1434fa052bae9377d1dad2bf8a7a52bc7b3b046d
Reetika paul <Reetikapaul67@gmail.com>
2025-02-04 15:32:01 +0530

    Update api2.py

--- api2.py
+++ api2.py
@@ -6,7 +6,7 @@ import requests
 city_name = input("Enter your city: ")
 api_key='bb4956b47038486784eb0193e3c7eeab'  #like passwords
 
-api_url =f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'  #We are going this url but with a specific city
+api_url =f'https://api.'your website'.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'  #We are going this url but with a specific city
 
 get_server_info = requests.get(api_url)
 data = get_server_info.json()
